bfab8bb2a26c778aa2411a9d4d35c704f9a61b58
Thabish <thabish@Thabishs-MacBook-Air.local>
2024-03-23 10:52:14 +0530

    new api folder created
    Signed-off-by: Thabish <thabish@Thabishs-MacBook-Air.local>

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const app = express();
+const PORT = 8000;
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://localhost:4000",
+    changeOrigin: true,
+  })
+);
+
+app.listen(PORT);
+
+export default app;
