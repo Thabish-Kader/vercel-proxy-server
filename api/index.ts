6e8d29a75fb3726a8bb557351e41a153d25e19d2
Thabish <thabish@Thabishs-MacBook-Air.local>
2024-03-24 10:29:26 +0530

    removed origin
    Signed-off-by: Thabish <thabish@Thabishs-MacBook-Air.local>

--- api/index.ts
+++ api/index.ts
@@ -2,16 +2,13 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
-const PORT = 8000;
 
 app.use(
   "/api",
   createProxyMiddleware({
     target: "https://22dc-115-96-183-112.ngrok-free.app",
-    changeOrigin: true,
+    changeOrigin: false,
   })
 );
-//22dc-115-96-183-112.ngrok-free.app
-https: app.listen(PORT);
 
 export default app;
